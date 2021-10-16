103d052d373a05529aed609a933c1550b6cc02c1
Josue Morales <josuemral10@gmail.com>
2021-10-15 22:03:46 -0600

    Getters y Setter de Motor y Carroceria

--- src/main/java/org/josue/Carroceria.java
+++ src/main/java/org/josue/Carroceria.java
@@ -1,2 +1,16 @@
-package org.josue;public class Carroceria {
+package org.josue;
+
+public abstract class Carroceria {
+
+    public abstract String presentaCR();
+
+    public String getCarroceria() {
+        return Carroceria;
+    }
+
+    public void setCarroceria(String carroceria) {
+        Carroceria = carroceria;
+    }
+
+    private String Carroceria;
 }
--- src/main/java/org/josue/Motor.java
+++ src/main/java/org/josue/Motor.java
@@ -1,2 +1,16 @@
-package org.josue;public class Motor {
+package org.josue;
+
+public abstract class Motor {
+    public abstract String presentaMT();
+
+    private String motor;
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
 }
